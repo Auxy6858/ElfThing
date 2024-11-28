a6ff3f831185ca0c90a25b0bfa37fe1f59a63d28
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2024-11-28 00:22:07 +0000

    Init

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ElfThing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: ElfThing.exe <input-file>, or drag and drop file onto the exe.");
+            goto end;
+        }
+
+        StreamReader textFile = new StreamReader(args[0]);
+        int total = 0;
+
+        string? currentLine = textFile.ReadLine();
+
+        while (currentLine != null)
+        {
+            total += GetNumberFromLine(currentLine);
+            currentLine = textFile.ReadLine();
+        }
+
+        Console.WriteLine(total);
+        end: ;
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    public static int GetNumberFromLine(string line)
+    {
+        int backPointer = line.Length - 1;
+        int frontPointer = 0;
+
+        while (!char.IsDigit(line[frontPointer]))
+        {
+                frontPointer++;
+        }
+
+        while (!char.IsDigit(line[backPointer]))
+        {
+            backPointer--;
+        }
+
+        return int.Parse(
+            line[frontPointer].ToString()) * 10
+               + int.Parse(line[backPointer].ToString()
+        );
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+﻿# ElfThing
+
+Piece of code I wrote for the 2023 advent of code.
+
+## Purpose
+Given multiple lines of strings, we assume each line has 2 digits, Eg:
+
+```aaa1bbbbb7cccc```
+
+In this case the digit for this line would be 17.
+
+This program takes a text file you drag and drop onto it, searches each line for the first and last digit, 
+multiplies the first digit by 10 and then adds this value to a variable, along with the second digit.
+This keeps count of the total value of all the numbers for each line.
